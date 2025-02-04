7cd1a365bc6312d16cbb11982b57463d31404b01
Stuart Forrest <stuart@beasley-forrest.net>
2025-02-03 19:53:37 -0500

    More work on main

--- Main/main.py
+++ Main/main.py
@@ -4,6 +4,7 @@ from PySide6 import QtWidgets as qtw
 from PySide6 import QtGui as qtg
 
 from Main.UI.main_window import Ui_w_main
+from Persons.UI.add_person_dialog import Ui_d_person
 
 class MainWindow(qtw.QMainWindow, Ui_w_main):
     def __init__(self):
@@ -11,6 +12,10 @@ class MainWindow(qtw.QMainWindow, Ui_w_main):
         self.setupUi(self)
 
         self.act_quit.triggered.connect(self.close)
+        self.act_add_person.triggered.connect(self.open_person_dialog)
+
+    def open_person_dialog(self):
+        print("Dialog Opened")
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
